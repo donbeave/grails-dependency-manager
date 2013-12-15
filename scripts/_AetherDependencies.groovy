c7a643e85736b059ff507338cfc65e9f8a0256ce
David Dawson <david.dawson@simplicityitself.com>
2013-12-15 22:50:21 +0000

    Aether usage was replacing, not adding to, the classpath.

--- scripts/_AetherDependencies.groovy
+++ scripts/_AetherDependencies.groovy
@@ -3,36 +3,45 @@ includeTargets << grailsScript("_GrailsInit")
 
 addTestDependency = { groupId, artifactId, version, type = "jar" ->
   def files = getDeps(groupId, artifactId, version, type)
-  grailsSettings.testDependencies = files
+  grailsSettings.testDependencies.addAll(files)
   return files
 }
 
 addProvidedDependency = { groupId, artifactId, version, type = "jar" ->
   def files = getDeps(groupId, artifactId, version, type)
-  grailsSettings.providedDependencies = files
+  grailsSettings.providedDependencies.addAll(files)
   return files
 }
 
 addCompileDependency = { groupId, artifactId, version, type = "jar" ->
   def files = getDeps(groupId, artifactId, version, type)
-  grailsSettings.compileDependencies = files
+  grailsSettings.compileDependencies.addAll(files)
   return files
 }
 
 addRuntimeDependency = { groupId, artifactId, version, type = "jar" ->
   def files = getDeps(groupId, artifactId, version, type)
-  grailsSettings.runtimeDependencies = files
+  grailsSettings.runtimeDependencies.addAll(files)
   return files
 }
 
 def getDeps( groupId, artifactId, version, type) {
   def loader = grailsSettings.dependencyManager.getClass().classLoader
 
-  def Dependency = loader.loadClass("org.sonatype.aether.graph.Dependency")
-  def DefaultArtifact = loader.loadClass("org.sonatype.aether.util.artifact.DefaultArtifact")
-
+  def Dependency
+  try {
+    Dependency = loader.loadClass("org.sonatype.aether.graph.Dependency")
+  } catch (ClassNotFoundException ex) {
+    Dependency = loader.loadClass("org.eclipse.aether.graph.Dependency")
+  }
+  def DefaultArtifact
+  try {
+    DefaultArtifact = loader.loadClass("org.sonatype.aether.util.artifact.DefaultArtifact")
+  } catch (ClassNotFoundException ex) {
+    DefaultArtifact = loader.loadClass("org.eclipse.aether.artifact.DefaultArtifact")
+  }
   final dependency = Dependency.newInstance(DefaultArtifact.newInstance(
-          groupId, artifactId, type, version), "compile")
+      groupId, artifactId, type, version), "compile")
 
   grailsSettings.dependencyManager.addDependency(dependency)
 
