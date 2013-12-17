6e0c7366fc32ef3c804a9491e9ac62385ce031a6
David Dawson <david.dawson@simplicityitself.com>
2013-12-17 22:33:23 +0000

    Tweak ivy to download all the deps and add them to the classpath, not just a few of them

--- scripts/_IvyDependencies.groovy
+++ scripts/_IvyDependencies.groovy
@@ -1,39 +1,43 @@
 includeTargets << grailsScript("_GrailsInit")
 
 addTestDependency = { group, name, version, type = null ->
-  getDeps(group, name, version, type).each { file ->
+  getDeps(group, name, version, "compile").each { file ->
     grailsSettings.testDependencies << file
     return [file]
   }
 }
 
 addProvidedDependency = { group, name, version, type = null ->
-  getDeps(group, name, version, type).each { file ->
+  getDeps(group, name, version, "compile").each { file ->
     grailsSettings.providedDependencies << file
     return [file]
   }
 }
 
 addCompileDependency = { group, name, version, type = null ->
-  getDeps(group, name, version, type).each { file ->
+  getDeps(group, name, version, "compile").each { file ->
     grailsSettings.compileDependencies << file
     return [file]
   }
 }
 
 addRuntimeDependency = { group, name, version, type = null ->
-  getDeps(group, name, version, type).each { file ->
+  getDeps(group, name, version, "compile").each { file ->
     grailsSettings.runtimeDependencies << file
     return [file]
   }
 }
 
-def getDeps(group, name, version, type) {
+def getDeps(group, name, version, type="jar") {
+
   def extraAttrs = type == null ? [:] : ['m:classifier': type]
 
   def ModuleRevisionId = grailsSettings.dependencyManager.getClass().classLoader.loadClass("org.apache.ivy.core.module.id.ModuleRevisionId")
 
-  def mrid = ModuleRevisionId.newInstance(group, name, version, extraAttrs)
+  def mrid = ModuleRevisionId.newInstance(group, name, version, [:])
+
+  println "DEPS ${mrid} // ${type}"
+
   addModuleToDependencies(mrid, 'compile')
 }
 
@@ -42,14 +46,17 @@ def addModuleToDependencies(mrid, type) {
   def ResolveOptions = grailsSettings.dependencyManager.getClass().classLoader.loadClass("org.apache.ivy.core.resolve.ResolveOptions")
   def DownloadOptions = grailsSettings.dependencyManager.getClass().classLoader.loadClass("org.apache.ivy.core.resolve.DownloadOptions")
 
-  def report = grailsSettings.dependencyManager.resolveEngine.resolve(mrid, ResolveOptions.newInstance(confs: [type] as String[], transitive:false, outputReport: true, download: true, useCacheOnly: false), false)
+  def report = grailsSettings.dependencyManager.resolveEngine.resolve(mrid, ResolveOptions.newInstance(confs: ["compile","master"] as String[], transitive:true, outputReport: true, download: true, useCacheOnly: false), false)
 
   if (report.hasError()) {
-    println "Ivy Extended Dependency resolution has errors, exiting"
+    println "Ivy Extended Dependency resolution has errors on artifact [${mrid}], exiting"
+    println report.getUnresolvedDependencies().collect {
+      it.problem.printStackTrace()
+    }
     exit(1)
   }
   report.artifacts.collect {
-    def rep = grailsSettings.dependencyManager.resolveEngine.download(it, DownloadOptions.newInstance(log: DownloadOptions.LOG_DOWNLOAD_ONLY))
+    def rep = grailsSettings.dependencyManager.resolveEngine.download(it, DownloadOptions.newInstance(log: DownloadOptions.LOG_DEFAULT))
     rep.localFile
   }
 }
